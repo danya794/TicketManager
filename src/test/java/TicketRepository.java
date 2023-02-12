7048bff11f9d8fbdf1d5b28229bc16da2c5ad2c2
Danil Kovalev <dannykvlv@gmail.com>
2023-02-12 17:36:32 +0300

    first commit

---
+++ src/test/java/TicketRepository.java
@@ -0,0 +1,17 @@
+public class TicketRepository {
+    private Ticket[] tickets = new Ticket[0];
+
+
+    public void add(Ticket ticket) {
+        Ticket[] tmp = new Ticket[tickets.length + 1];
+        for (int i = 0; i < tickets.length; i++) {
+            tmp[i] = tickets[i];
+        }
+        tmp[tmp.length - 1] = ticket;
+        tickets = tmp;
+    }
+
+    public Ticket[] findAll() {
+        return tickets;
+    }
+}
